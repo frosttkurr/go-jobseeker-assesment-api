393e5ce8c9b15ef270fa065fe8d4974e3e8e8b1b
frosttkurr <thisiscyber13@gmail.com>
2023-08-20 04:54:48 +0800

    create models, controllers, and logic for index/get data

--- controllers/candidate_controller.go
+++ controllers/candidate_controller.go
@@ -1,1 +1,31 @@
 package controllers
+
+import (
+	"go-jobseeker-assesment-fullstack/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Index(c *gin.Context) {
+	var response gin.H
+	candidates, err := models.GetCandidates()
+
+	if err == nil {
+		response = gin.H{
+			"meta": gin.H{
+				"status":  200,
+				"message": "Successfully get data",
+			},
+			"result": candidates,
+		}
+		c.JSON(200, response)
+	} else {
+		response = gin.H{
+			"meta": gin.H{
+				"status":  500,
+				"message": "Failed to get data",
+			},
+		}
+		c.JSON(500, response)
+	}
+}
--- models/t_candidate.go
+++ models/t_candidate.go
@@ -1,5 +1,7 @@
 package models
 
+import "go-jobseeker-assesment-fullstack/initializers"
+
 type T_Candidate struct {
 	Candidate_ID uint   `gorm:"primary_key" json:"candidate_id"`
 	Email        string `gorm:"type:varchar(255);unique_index;not null" json:"email"`
@@ -10,4 +12,12 @@ type T_Candidate struct {
 	Gender       string `gorm:"type:char(1);check:gender IN ('F', 'M')" json:"gender"`
 	Year_Exp     string `gorm:"type:varchar(10);not null" json:"year_exp"`
 	Last_Salary  string `gorm:"type:varchar(255)" json:"last_salary"`
+}
+
+func GetCandidates() (results []T_Candidate, err error) {
+	db := initializers.DB.Raw("SELECT * FROM t_candidate").Scan(&results)
+	if db.Error != nil {
+		err = db.Error
+	}
+	return results, err
 }
