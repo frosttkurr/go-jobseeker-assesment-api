4efb868b7a9fa485e8e605f216c33dbd4ea98463
frosttkurr <thisiscyber13@gmail.com>
2023-08-20 07:38:58 +0800

    add validation for gender

--- models/t_candidate.go
+++ models/t_candidate.go
@@ -4,19 +4,20 @@ import (
 	"errors"
 	"go-jobseeker-assesment-api/initializers"
 
+	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
 type T_Candidate struct {
-	Candidate_ID uint   `gorm:"primary_key" json:"candidate_id"`
-	Email        string `gorm:"type:varchar(255);unique_index;not null" json:"email" validate:"required"`
-	Phone_Number string `gorm:"type:varchar(20);unique_index" json:"phone_number"`
-	Full_Name    string `gorm:"type:varchar(255);not null" json:"full_name" validate:"required"`
-	DOB          string `gorm:"type:varchar(10)" json:"dob" validate:"required"`
-	POB          string `gorm:"type:varchar(255)" json:"pob" validate:"required"`
-	Gender       string `gorm:"type:char(1);check:gender IN ('F', 'M')" json:"gender" validate:"required"`
-	Year_Exp     string `gorm:"type:varchar(10);not null" json:"year_exp" validate:"required"`
-	Last_Salary  string `gorm:"type:varchar(255)" json:"last_salary"`
+	Candidate_ID uint   `json:"candidate_id"`
+	Email        string `json:"email" validate:"required,email"`
+	Phone_Number string `json:"phone_number"`
+	Full_Name    string `json:"full_name" validate:"required"`
+	DOB          string `json:"dob" validate:"required"`
+	POB          string `json:"pob" validate:"required"`
+	Gender       string `json:"gender" validate:"required"`
+	Year_Exp     string `json:"year_exp" validate:"required"`
+	Last_Salary  string `json:"last_salary" validate:"required"`
 }
 
 func GetCandidates() (results []T_Candidate, err error) {
@@ -39,7 +40,12 @@ func InsertCandidate(data T_Candidate) (err error) {
 	var db *gorm.DB
 	var count_email, count_phone_number int64
 
-	db = initializers.DB.Raw("SELECT COUNT(candidate_id) AS id FROM t_candidate WHERE email = ?", data.Email).Count(&count_email)
+	validate := validator.New()
+	if err := validate.Struct(data); err != nil {
+		return err.(validator.ValidationErrors)
+	}
+
+	db = initializers.DB.Raw("SELECT COUNT(candidate_id) AS id FROM t_candidate WHERE email = ?", data.Email).Scan(&count_email)
 	if db.Error != nil {
 		return db.Error
 	}
@@ -48,13 +54,17 @@ func InsertCandidate(data T_Candidate) (err error) {
 		return errors.New("ERROR: Email must be unique")
 	}
 
-	db = initializers.DB.Raw("SELECT COUNT(candidate_id) AS id FROM t_candidate WHERE phone_number = ?", data.Phone_Number).Count(&count_phone_number)
+	db = initializers.DB.Raw("SELECT COUNT(candidate_id) AS id FROM t_candidate WHERE phone_number = ?", data.Phone_Number).Scan(&count_phone_number)
 	if db.Error != nil {
 		return db.Error
 	}
 
 	if count_phone_number > 0 {
 		return errors.New("ERROR: Phone number must be unique")
+	}
+
+	if data.Gender != "M" && data.Gender != "F" {
+		return errors.New("ERROR: Gender must be male (M) or female (F)")
 	}
 
 	candidate := T_Candidate{
