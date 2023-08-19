c2e0a6553e2dcca798c2f0e2b1a8c58162af9eb5
frosttkurr <thisiscyber13@gmail.com>
2023-08-20 05:06:29 +0800

    add route for get index data

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"go-jobseeker-assesment-api/controllers"
+	"go-jobseeker-assesment-api/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnv()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	r := gin.Default()
+
+	api := r.Group("api")
+	{
+		candidates := api.Group("/candidates")
+		{
+			candidates.GET("/", controllers.Index)
+		}
+	}
+
+	r.Run()
+}
